318f52917763163b09a38c6082353a4aea813a52
LAS0324 <as125230as@gmail.com>
2025-05-16 10:47:38 +0800

    Photoresistor
    Photoresistor Group14

--- RotaryPotentiometer.ino
+++ RotaryPotentiometer.ino
@@ -3,13 +3,18 @@ int analogPin = A0;    // potentiometer connected to analog pin A0
 int val = 0;           // variable to store the read value
 
 void setup() {
-  Serial.begin(9600);
+  
   pinMode(ledPin, OUTPUT);  // sets the pin as output
+  Serial.begin(9600);
 }
 
 void loop() {
   val = analogRead(analogPin);  // read the input pin
-  analogWrite(ledPin, val / 4); // scale from 0–1023 to 0–255
+  
   Serial.println(val);          // print the value
+  if(val<500) val=500;
+  if(val>800) val=800;
+  int leddimming = map(val,500,800,0,100);
+  analogWrite(ledPin,leddimming); // scale from 0–1023 to 0–255
   delay(1);                     // delay for stability
 }
